f726f5cd6791b289fb4c37ec90aac65ba0117f64
gauravd-123 <gauravdabholkar410@gmail.com>
2024-11-24 20:36:20 +0530

    Test push: HealthCheck

---
+++ VideoAPI/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+        public LoginController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string healthCheck()
+        {
+            return "Working Fine";
+        }
+    }
+}
